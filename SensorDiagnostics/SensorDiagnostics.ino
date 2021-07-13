ee048d1babf6382ceff3cad7b471df7b8b1a6807
Lily-Dorstewitz <lily@dorstewitz.us>
2021-07-13 11:42:33 -0400

    Update SensorDiagnostics.ino

--- SensorDiagnostics/SensorDiagnostics.ino
+++ SensorDiagnostics/SensorDiagnostics.ino
@@ -19,44 +19,49 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
   // read the input on analog pin 0:
-  int LeftValue = analogRead(A5);
+  int LeftValue = analogRead(A0);
   // print out the value you read:
-  Serial.print ("Left = ");
-  Serial.print (LeftValue);
+ // Serial.print (" Left = ");
+  Serial.println (LeftValue);
   //Average n readings of this sensor
-  float totalValue = 0;
-  int n = 100;
+ /* float totalValue = 0;
+  int n = 20;
+  unsigned long timeBefore = millis();
   for(int i=0; i<(n-1); i++)
   {
-    LeftValue = analogRead(A5);
+    LeftValue = analogRead(A0);
     totalValue = totalValue + LeftValue;
   }
   float avgLeftValue = totalValue/n;
+  unsigned long timeAfter = millis();
+  float timeToRun = (timeAfter - timeBefore);
+  Serial.print (" Time to calculate is ");
+  Serial.println (timeToRun);
  
   
   //Right
   int RightValue = analogRead(A4);
-  Serial.print ("Right = ");
+  Serial.print (" Right = ");
   Serial.print (RightValue);
   //Up
   int UpValue = analogRead(A1);
-  Serial.print ("Up = ");
+  Serial.print (" Up = ");
   Serial.print (UpValue);
   //Down
   int DownValue = analogRead(A3);
-  Serial.print ("Down = ");
+  Serial.print (" Down = ");
   Serial.print (DownValue);
   //Forward
   int ForwardValue = analogRead(A2);
-  Serial.print ("Forward = ");
+  Serial.print (" Forward = ");
   Serial.print (ForwardValue);
   //Backward
-  int BackwardValue = analogRead(A0);
-  Serial.print ("Backward = ");
+  int BackwardValue = analogRead(A5);
+  Serial.print (" Backward = ");
   Serial.print (BackwardValue);
   Serial.println (" ");
   Serial.print ("Average Left Value is ");
   Serial.println (avgLeftValue);
-  
-  delay(100);        // delay in between reads for stability
+  */
+  delay(50);        // delay in between reads for stability
 }
